a51a058ed7b828f8ebbc8f0e4a49fdad95bdff9b
rahulbarve <56666530+rahulbarve@users.noreply.github.com>
2025-07-26 21:27:24 +0530

    Update RestProjectApplication.java

--- Rest_Project/src/main/java/example/rest/RestProjectApplication.java
+++ Rest_Project/src/main/java/example/rest/RestProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("WELCOME TO SPRING REST APPLICATION");
 		SpringApplication.run(RestProjectApplication.class, args);
 	}
 

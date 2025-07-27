5622c74af7906bf179a3ba17efcd6ef4f0e29070
rahulbarve <56666530+rahulbarve@users.noreply.github.com>
2025-07-27 06:43:04 +0530

    Update RestImpl.java

--- Rest_Project/src/main/java/example/rest/RestImpl.java
+++ Rest_Project/src/main/java/example/rest/RestImpl.java
@@ -10,7 +10,7 @@ public class RestImpl {//This class is meant for exposing REST Endpoints
 	//End Point: /do-greet => Response: "Welcome to REST"
 	@RequestMapping("/do-greet")//Marks this method as Request Handling method.
 	public String doGreet() {
-		return "Welcome to my first REST demo. Thank you once again!!";
+		return "Welcome to my first REST demo. Thank you. Visit again.";
 	}
 	
 	//End Point: /get-message => Response: Message Object (content, deliveredOn)
